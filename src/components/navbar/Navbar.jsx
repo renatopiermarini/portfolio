2a2e7bd94e2c74e070b7aa195aa6875027e75445
Renato Piermarini <renatopiermarinih@gmail.com>
2022-03-04 12:37:50 -0300

    navbar routes conditional

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,26 @@
 import "./navbar.css";
 import logo from "../pictures/logo.jpeg";
 import { NavLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav className="navbar" id="top">
       <div className="navbar-div">
         <NavLink to="/">
           <img src={logo} className="logo" />
         </NavLink>
-        <a className="say-hello" href="#contact">
-          Say Hello
-        </a>
+        {location.pathname === "/" ? (
+          <a className="say-hello" href="#contact">
+            Say Hello
+          </a>
+        ) : (
+          <NavLink className="say-hello" to="/">
+            Home
+          </NavLink>
+        )}
       </div>
     </nav>
   );
